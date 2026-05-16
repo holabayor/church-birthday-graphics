44502f7af26e4e6263d5b9bdbf423f17b2fcbb34
holabayor <liasu.olabayo@gmail.com>
2026-05-16 09:30:22 +0100

    update designs

--- src/lib/designs.tsx
+++ src/lib/designs.tsx
@@ -54,155 +54,6 @@ export const designs: Array<{
   render: (props: DesignProps) => React.ReactElement;
 }> = [
   {
-    name: "Royal Ivory",
-    render: ({ member, message, churchLogoUrl }) => (
-      <div
-        style={{
-          display: "flex",
-          width: 1080,
-          height: 1080,
-          background: "#f6f1e8",
-          position: "relative",
-          overflow: "hidden",
-          fontFamily: "sans-serif",
-        }}
-      >
-        {/* Decorative Shape */}
-        <div
-          style={{
-            display: "flex",
-            position: "absolute",
-            width: 700,
-            height: 700,
-            borderRadius: 350,
-            background: "#ece2cf",
-            top: -250,
-            right: -250,
-          }}
-        />
-
-        {/* Left */}
-        <div
-          style={{
-            display: "flex",
-            width: "56%",
-            flexDirection: "column",
-            justifyContent: "center",
-            padding: 80,
-          }}
-        >
-          {churchLogoUrl && (
-            <div style={{ display: "flex", marginBottom: 50 }}>
-              <img src={churchLogoUrl} alt="" height={110} style={{ objectFit: "contain" }} />
-            </div>
-          )}
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 22,
-              letterSpacing: 6,
-              color: "#b88b2c",
-              marginBottom: 24,
-            }}
-          >
-            HAPPY BIRTHDAY
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 78,
-              fontWeight: 800,
-              lineHeight: 1,
-              color: "#1f1f1f",
-              marginBottom: 28,
-            }}
-          >
-            {getFullName(member)}
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              width: 120,
-              height: 5,
-              background: "#c9a227",
-              marginBottom: 36,
-            }}
-          />
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 29,
-              lineHeight: 1.6,
-              color: "#555",
-              marginBottom: 36,
-            }}
-          >
-            {message}
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 30,
-              color: "#b88b2c",
-              fontWeight: 700,
-            }}
-          >
-            {getBirthDate(member)}
-          </div>
-        </div>
-
-        {/* Right */}
-        <div
-          style={{
-            display: "flex",
-            width: "44%",
-            justifyContent: "center",
-            alignItems: "center",
-            position: "relative",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              width: 430,
-              height: 620,
-              borderRadius: 220,
-              overflow: "hidden",
-              border: "8px solid white",
-            }}
-          >
-            {member.photo_url ? (
-              <img
-                src={member.photo_url}
-                alt=""
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-            ) : (
-              <div
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  height: "100%",
-                  background: "#ddd",
-                }}
-              />
-            )}
-          </div>
-        </div>
-      </div>
-    ),
-  },
-
-  {
     name: "Midnight Gold",
     render: ({ member, message, churchLogoUrl }) => (
       <div
@@ -257,7 +108,7 @@ export const designs: Array<{
               marginBottom: 20,
             }}
           >
-            CELEBRATING LIFE
+            HAPPY BIRTHDAY
           </div>
 
           <div
@@ -344,139 +195,6 @@ export const designs: Array<{
                 }}
               />
             )}
-          </div>
-        </div>
-      </div>
-    ),
-  },
-
-  {
-    name: "Sky Grace",
-    render: ({ member, message, churchLogoUrl }) => (
-      <div
-        style={{
-          display: "flex",
-          width: 1080,
-          height: 1080,
-          background: "#eef6ff",
-          fontFamily: "sans-serif",
-          overflow: "hidden",
-        }}
-      >
-        {/* Photo */}
-        <div
-          style={{
-            display: "flex",
-            width: "48%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              width: 470,
-              height: 700,
-              borderRadius: 240,
-              overflow: "hidden",
-              border: "10px solid white",
-            }}
-          >
-            {member.photo_url ? (
-              <img
-                src={member.photo_url}
-                alt=""
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-            ) : (
-              <div
-                style={{
-                  display: "flex",
-                  width: "100%",
-                  height: "100%",
-                  background: "#ddd",
-                }}
-              />
-            )}
-          </div>
-        </div>
-
-        {/* Text */}
-        <div
-          style={{
-            display: "flex",
-            width: "52%",
-            flexDirection: "column",
-            justifyContent: "center",
-            paddingRight: 80,
-          }}
-        >
-          {churchLogoUrl && (
-            <div style={{ display: "flex", marginBottom: 50 }}>
-              <img src={churchLogoUrl} alt="" height={100} />
-            </div>
-          )}
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 22,
-              color: "#2563eb",
-              letterSpacing: 5,
-              marginBottom: 22,
-            }}
-          >
-            HAPPY BIRTHDAY
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 80,
-              lineHeight: 1,
-              fontWeight: 800,
-              color: "#111827",
-              marginBottom: 30,
-            }}
-          >
-            {getFullName(member)}
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              width: 120,
-              height: 5,
-              background: "#2563eb",
-              marginBottom: 35,
-            }}
-          />
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 29,
-              lineHeight: 1.7,
-              color: "#4b5563",
-              marginBottom: 40,
-            }}
-          >
-            {message}
-          </div>
-
-          <div
-            style={{
-              display: "flex",
-              fontSize: 30,
-              color: "#2563eb",
-              fontWeight: 700,
-            }}
-          >
-            {getBirthDate(member)}
           </div>
         </div>
       </div>
@@ -578,7 +296,7 @@ export const designs: Array<{
               marginBottom: 20,
             }}
           >
-            BLESSED CELEBRATION
+            HAPPY BIRTHDAY
           </div>
 
           <div
@@ -629,9 +347,8 @@ export const designs: Array<{
       </div>
     ),
   },
-
   {
-    name: "Minimal White Gold",
+    name: "Crimson Velvet",
     render: ({ member, message, churchLogoUrl }) => (
       <div
         style={{
@@ -639,13 +356,14 @@ export const designs: Array<{
           flexDirection: "column",
           width: 1080,
           height: 1080,
-          background: "white",
+          background: "#310a14", // Deep Crimson
           overflow: "hidden",
           fontFamily: "sans-serif",
           alignItems: "center",
+          position: "relative",
         }}
       >
-        {/* Top Bar */}
+        {/* Top Gold Bar */}
         <div
           style={{
             display: "flex",
@@ -661,14 +379,15 @@ export const designs: Array<{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            paddingTop: 50,
+            paddingTop: 60,
             paddingLeft: 80,
             paddingRight: 80,
+            zIndex: 10,
           }}
         >
           {churchLogoUrl && (
             <div style={{ display: "flex", marginBottom: 35 }}>
-              <img src={churchLogoUrl} alt="" height={90} />
+              <img src={churchLogoUrl} alt="Church Logo" height={90} style={{ objectFit: "contain" }} />
             </div>
           )}
 
@@ -679,14 +398,14 @@ export const designs: Array<{
               height: 400,
               borderRadius: 200,
               overflow: "hidden",
-              border: "8px solid #f5e7b2",
+              border: "8px solid #d4af37",
               marginBottom: 40,
             }}
           >
             {member.photo_url ? (
               <img
                 src={member.photo_url}
-                alt=""
+                alt="Member"
                 style={{
                   width: "100%",
                   height: "100%",
@@ -699,7 +418,7 @@ export const designs: Array<{
                   display: "flex",
                   width: "100%",
                   height: "100%",
-                  background: "#ddd",
+                  background: "#4a1220",
                 }}
               />
             )}
@@ -710,7 +429,7 @@ export const designs: Array<{
               display: "flex",
               fontSize: 22,
               letterSpacing: 5,
-              color: "#a16207",
+              color: "#e8b943",
               marginBottom: 20,
             }}
           >
@@ -724,7 +443,7 @@ export const designs: Array<{
               fontSize: 76,
               lineHeight: 1,
               fontWeight: 800,
-              color: "#111827",
+              color: "white",
               marginBottom: 28,
             }}
           >
@@ -747,7 +466,7 @@ export const designs: Array<{
               textAlign: "center",
               fontSize: 28,
               lineHeight: 1.7,
-              color: "#4b5563",
+              color: "#e5e7eb",
               marginBottom: 36,
             }}
           >
@@ -758,7 +477,7 @@ export const designs: Array<{
             style={{
               display: "flex",
               fontSize: 30,
-              color: "#a16207",
+              color: "#d4af37",
               fontWeight: 700,
             }}
           >
@@ -768,11 +487,304 @@ export const designs: Array<{
       </div>
     ),
   },
+
+  {
+    name: "Royal Sapphire",
+    render: ({ member, message, churchLogoUrl }) => (
+      <div
+        style={{
+          display: "flex",
+          width: 1080,
+          height: 1080,
+          background: "#0a192f", // Deep Navy Blue
+          fontFamily: "sans-serif",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <DotPattern color="rgba(56, 189, 248, 0.05)" />
+
+        {/* Photo Left */}
+        <div
+          style={{
+            display: "flex",
+            width: "48%",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingLeft: 20,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              width: 440,
+              height: 680,
+              borderRadius: 30,
+              overflow: "hidden",
+              border: "6px solid #38bdf8",
+              boxShadow: "0 20px 40px rgba(0,0,0,0.4)",
+            }}
+          >
+            {member.photo_url ? (
+              <img
+                src={member.photo_url}
+                alt="Member"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  height: "100%",
+                  background: "#112240",
+                }}
+              />
+            )}
+          </div>
+        </div>
+
+        {/* Text Right */}
+        <div
+          style={{
+            display: "flex",
+            width: "52%",
+            flexDirection: "column",
+            justifyContent: "center",
+            paddingRight: 80,
+            paddingLeft: 30,
+            zIndex: 10,
+          }}
+        >
+          {churchLogoUrl && (
+            <div style={{ display: "flex", marginBottom: 50 }}>
+              <img src={churchLogoUrl} alt="Church Logo" height={100} style={{ objectFit: "contain" }} />
+            </div>
+          )}
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 22,
+              color: "#38bdf8", // Vibrant Sky Blue accent
+              letterSpacing: 5,
+              marginBottom: 22,
+            }}
+          >
+            HAPPY BIRTHDAY
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 80,
+              lineHeight: 1,
+              fontWeight: 800,
+              color: "white",
+              marginBottom: 30,
+            }}
+          >
+            {getFullName(member)}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              width: 120,
+              height: 5,
+              background: "#38bdf8",
+              marginBottom: 35,
+            }}
+          />
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 29,
+              lineHeight: 1.7,
+              color: "#cbd5e1",
+              marginBottom: 40,
+            }}
+          >
+            {message}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 30,
+              color: "#38bdf8",
+              fontWeight: 700,
+            }}
+          >
+            {getBirthDate(member)}
+          </div>
+        </div>
+      </div>
+    ),
+  },
+
+  {
+    name: "Amethyst Night",
+    render: ({ member, message, churchLogoUrl }) => (
+      <div
+        style={{
+          display: "flex",
+          width: 1080,
+          height: 1080,
+          background: "#1a0b2e", // Deep Plum/Purple
+          position: "relative",
+          overflow: "hidden",
+          fontFamily: "sans-serif",
+        }}
+      >
+        {/* Decorative Ambient Shapes */}
+        <div
+          style={{
+            display: "flex",
+            position: "absolute",
+            width: 800,
+            height: 800,
+            borderRadius: 400,
+            background: "#2d1b4e",
+            top: -300,
+            right: -200,
+            opacity: 0.8,
+          }}
+        />
+
+        {/* Text Left */}
+        <div
+          style={{
+            display: "flex",
+            width: "56%",
+            flexDirection: "column",
+            justifyContent: "center",
+            padding: 80,
+            zIndex: 10,
+          }}
+        >
+          {churchLogoUrl && (
+            <div style={{ display: "flex", marginBottom: 50 }}>
+              <img src={churchLogoUrl} alt="Church Logo" height={110} style={{ objectFit: "contain" }} />
+            </div>
+          )}
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 22,
+              letterSpacing: 6,
+              color: "#fbbf24", // Warm Gold
+              marginBottom: 24,
+            }}
+          >
+            HAPPY BIRTHDAY
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 78,
+              fontWeight: 800,
+              lineHeight: 1,
+              color: "white",
+              marginBottom: 28,
+            }}
+          >
+            {getFullName(member)}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              width: 120,
+              height: 5,
+              background: "#fbbf24",
+              marginBottom: 36,
+            }}
+          />
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 29,
+              lineHeight: 1.6,
+              color: "#d1d5db",
+              marginBottom: 36,
+            }}
+          >
+            {message}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              fontSize: 30,
+              color: "#fbbf24",
+              fontWeight: 700,
+            }}
+          >
+            {getBirthDate(member)}
+          </div>
+        </div>
+
+        {/* Arch Photo Right */}
+        <div
+          style={{
+            display: "flex",
+            width: "44%",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+            zIndex: 10,
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              width: 420,
+              height: 640,
+              borderRadius: "210px 210px 20px 20px", // Archway shape
+              overflow: "hidden",
+              border: "6px solid #fbbf24",
+              boxShadow: "-10px 10px 30px rgba(0,0,0,0.5)",
+            }}
+          >
+            {member.photo_url ? (
+              <img
+                src={member.photo_url}
+                alt="Member"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  height: "100%",
+                  background: "#3b1b54",
+                }}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    ),
+  },
 ];
 
 export const defaultMessages = [
   // "Wishing you a wonderful birthday filled with God's grace and blessings. May this new year of your life be filled with joy, peace, and abundant love!",
-  "Happy Birthday! May the Lord continue to bless you and keep you. May His face shine upon you and give you peace throughout this new year.",
+  "May the Lord continue to bless you and keep you. May His face shine upon you and give you peace throughout this new year.",
   // "On your special day, we celebrate the gift you are to our church family. May God's blessings overflow in your life today and always!",
   // "Rejoice, for this is the day the Lord has made! Happy Birthday! May you be blessed with good health, happiness, and divine favor.",
   // "Happy Birthday! As you mark another year of God's faithfulness, may you continue to grow in grace and in the knowledge of our Lord Jesus Christ.",
